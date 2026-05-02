a55e90ebd26ee0b393d5a3fc91a375704b61e9de
shirokuroiori <shirokurousa123@gmail.com>
2026-05-03 07:11:40 +0900

    word hilight

---
+++ .config/nvim/lua/plugins/illuminate.lua
@@ -0,0 +1,14 @@
+return {
+  "RRethy/vim-illuminate",
+  event = { "BufReadPost", "BufNewFile" },
+  opts = {
+    delay = 100,
+    large_file_cutoff = 2000,
+  },
+  config = function(_, opts)
+    require("illuminate").configure(opts)
+    vim.api.nvim_set_hl(0, "IlluminatedWordText",  { bg = "#3d3d3d" })
+    vim.api.nvim_set_hl(0, "IlluminatedWordRead",  { bg = "#3d3d3d" })
+    vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { bg = "#3d3d3d" })
+  end,
+}
